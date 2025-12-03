639a6811cc945a08a26f4f89e4501bb4f9db7cff
MiN <81948815+leesumin0526@users.noreply.github.com>
2025-12-03 21:17:29 +1100

    [Setting] 프로젝트 세팅

---
+++ src/test/java/com/dev/tripcraft/TripcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.tripcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
